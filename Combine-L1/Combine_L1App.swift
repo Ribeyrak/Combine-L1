c255506e202ef81294f9eb30d36f982f0b77124a
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-09-06 18:16:35 -0400

    Initial Commit

---
+++ Combine-L1/Combine_L1App.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_L1App.swift
+//  Combine-L1
+//
+//  Created by Evhen Lukhtan on 06.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Combine_L1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
